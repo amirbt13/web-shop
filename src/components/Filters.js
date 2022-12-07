67ac0ee76ad1c1938acf07d85359d1bb87fce800
Amir Basirati <amirbasirati13@gmail.com>
2022-12-07 18:13:55 +0330

    search bar added to filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,11 +3,13 @@ import React, { useState } from 'react'
 import arrow from '../icons/down-arrow.svg'
 import arrowWhite from '../icons/down-arrow-white.svg'
 
+
 const Filters = ({ setProducts }) => {
   
   const [filterShow, setFilterShow] = useState({
     mainSection: false,
-    category: false
+    category: false,
+    search: false
   })
 
 
@@ -42,6 +44,31 @@ const Filters = ({ setProducts }) => {
 
   }
 
+  const searchHandler = (event) => {
+    
+    if(event.target.value !== ""){
+
+      setProducts(prevProducts => prevProducts.map(product => {
+
+        if(product.title.toLowerCase().includes(event.target.value)){
+          return {
+            ...product,
+            isShow: true
+          }
+        } else {
+          return {
+            ...product,
+            isShow: false
+          }
+        }
+      }))
+    } else {
+      setProducts(prevProducts => prevProducts.map(product => {
+        return {...product, isShow: true}
+      }))
+    }
+  }
+
 
 
 
@@ -56,44 +83,64 @@ const Filters = ({ setProducts }) => {
       })}>Filters
         <img className='ml-1 mt-1 w-4' src={arrowWhite} alt='arrow-down' />
       </div>
-      
-      <form className={`${filterShow.mainSection ? "block" : "hidden"} sm:block bg-white text-gray-800 mb-2 mx-1 rounded-lg py-1 px-2`}>
-        <div className='flex justify-between'
-        onClick={() => setFilterShow(prevFilterShow => {
-          return {
-            ...prevFilterShow,
-            category: !prevFilterShow.category
-          }
-        })}
-        >
-            <p>Categories </p>
-            <img className=' w-4' src={arrow} alt='arrow-down' />
-        </div>
-        <section className={`${filterShow.category ? "flex" : "hidden"} flex-col`}>
-         <div className='text-right'>
-         <div> 
-          <label className='mr-2'>All</label>
-          <input onClick={inputHandler} type="radio" name='category' defaultChecked='true' value="all"/>
-         </div>
-         <div>
-          <label className='mr-2'>men's clothing</label>
-          <input onClick={inputHandler} type="radio" name='category' value="men's clothing"/>
-         </div>
-         <div>
-          <label className='mr-2'>jewelery</label>
-          <input onClick={inputHandler} type="radio" name='category' value="jewelery"/>
-         </div>
-         <div>
-          <label className='mr-2'>electronics</label>
-          <input onClick={inputHandler} type="radio" name='category' value="electronics"/>
-         </div>
-         <div>
-          <label className='mr-2'>women's clothing</label>
-          <input onClick={inputHandler} type="radio" name='category' value="women's clothing"/>
-         </div>
-         </div>
-        </section>
-      </form>
+      <section className={`${filterShow.mainSection ? "flex" : "hidden"} sm:flex flex-col-reverse`}>
+          <form className={` bg-white text-gray-800 mb-2 mx-1 rounded-lg py-1 px-2`}>
+            <div className='flex justify-between'
+            onClick={() => setFilterShow(prevFilterShow => {
+              return {
+                ...prevFilterShow,
+                category: !prevFilterShow.category
+              }
+            })}
+            >
+                <p>Categories </p>
+                <img className=' w-4' src={arrow} alt='arrow-down' />
+            </div>
+            <div className={`${filterShow.category ? "flex" : "hidden"} flex-col`}>
+                <div className='text-right'>
+                    <div> 
+                      <label className='mr-2'>All</label>
+                      <input onClick={inputHandler} type="radio" name='category' defaultChecked='true' value="all"/>
+                    </div>
+                    <div>
+                      <label className='mr-2'>men's clothing</label>
+                      <input onClick={inputHandler} type="radio" name='category' value="men's clothing"/>
+                    </div>
+                    <div>
+                      <label className='mr-2'>jewelery</label>
+                      <input onClick={inputHandler} type="radio" name='category' value="jewelery"/>
+                    </div>
+                    <div>
+                      <label className='mr-2'>electronics</label>
+                      <input onClick={inputHandler} type="radio" name='category' value="electronics"/>
+                    </div>
+                    <div>
+                      <label className='mr-2'>women's clothing</label>
+                      <input onClick={inputHandler} type="radio" name='category' value="women's clothing"/>
+                    </div>
+                </div>
+            </div>
+          </form>
+          <form>
+            <div className='bg-white mb-2 mx-1 py-1 px-2 rounded-lg text-gray-800'>
+              <div className='flex justify-between'
+              onClick={() => setFilterShow(prevFilterShow => {
+                return {
+                  ...prevFilterShow,
+                  search: !prevFilterShow.search
+                }
+              })}
+              >
+                <p>Serach</p>
+                <img className='w-4' src={arrow} alt='arrow-down' />
+              </div>
+              <div className={`${filterShow.search ? "block" : "hidden"} mt-1`}>
+                <input className='w-full px-2 py-1 my-1 bg-slate-300 rounded-lg' type="text"
+                onChange={searchHandler}/>
+              </div>
+            </div>
+          </form>
+      </section>
     </div>
   )
 }
